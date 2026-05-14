ba07e3faf0b74940dd6bbbd9a74c3024e45436c6
rayyanbaig <rayyan53baig@gmail.com>
2026-05-14 22:34:13 +0530

    copy_from_user

---
+++ char_driver3/test4/driver.c
@@ -0,0 +1,83 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/kdev_t.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+
+int base_minor = 0;
+char* devname = "mychardev";
+int count =1;
+dev_t devnum;
+
+static struct class* class = NULL;
+static struct device* device = NULL;
+static struct cdev mycdev;
+
+static int device_open(struct inode* inode, struct file* file){
+	pr_info("%s\n", __func__);
+	return 0;
+}
+
+static ssize_t device_read(struct file* file, char __user* buffer, size_t length, loff_t* offset){
+	pr_info("%s:count:%lu \t offset:%llu\n", __func__, length, *offset);
+	//pr_info("%s\n", __func__);
+	return 0;
+}
+
+static ssize_t device_write(struct file* file, const char __user* buffer, size_t length, loff_t* offset){
+	char k_buffer[100] = {0};
+	int retval;
+	pr_info("%s: kernel buffer:%p\t User buffer:%p\n", __func__, k_buffer, buffer);
+	retval = copy_from_user(k_buffer, buffer, length);
+	pr_info("%s: copy from user returned: %d\n", __func__, retval);
+	pr_info("%s: kernel buffer: %s\t count:%lu \t offset:%llu\n", __func__, k_buffer, length, *offset);
+	//pr_info("%s\n", __func__);
+	return length;
+}
+
+static int device_release(struct inode* inode, struct file* file){
+	pr_info("%s\n", __func__);
+	return 0;
+}
+
+struct file_operations fops ={
+	.open = device_open,
+	.read = device_read,
+	.write = device_write,
+	.release = device_release
+};
+
+static int __init test_init(void){
+	class = class_create("myclass");
+
+	if(!alloc_chrdev_region(&devnum, base_minor, count, devname)){
+		printk("device numgistered\n");
+		printk("Major numbigned: %d\n", MAJOR(devnum));
+
+		device = device_create(class, NULL, devnum, NULL, devname);
+		cdev_init(&mycdev, &fops);
+		mycdev.owner = THIS_MODULE;
+		cdev_add(&mycdev, devnum, count);
+
+	}
+	else{
+		printk("device reg failed");
+	}
+	return 0;
+}
+
+static void __exit test_exit(void){
+	device_destroy(class, devnum);
+	class_destroy(class);
+
+	cdev_del(&mycdev);
+	unregister_chrdev_region(devnum, count);
+
+}
+
+module_init(test_init);
+module_exit(test_exit);
+
+MODULE_LICENSE("GPL");
+
---
+++ char_driver3/test4/driver.mod.c
@@ -0,0 +1,90 @@
+#include <linux/module.h>
+#include <linux/export-internal.h>
+#include <linux/compiler.h>
+
+MODULE_INFO(name, KBUILD_MODNAME);
+
+__visible struct module __this_module
+__section(".gnu.linkonce.this_module") = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+
+
+static const struct modversion_info ____versions[]
+__used __section("__versions") = {
+	{ 0x9f222e1e, "alloc_chrdev_region" },
+	{ 0xe486c4b7, "device_create" },
+	{ 0xd5f66efd, "cdev_init" },
+	{ 0x8ea73856, "cdev_add" },
+	{ 0x1595e410, "device_destroy" },
+	{ 0xa1dacb42, "class_destroy" },
+	{ 0x4e54d6ac, "cdev_del" },
+	{ 0x0bc5fb0d, "unregister_chrdev_region" },
+	{ 0xbd03ed67, "__ref_stack_chk_guard" },
+	{ 0x546c19d9, "validate_usercopy_range" },
+	{ 0xa61fd7aa, "__check_object_size" },
+	{ 0x092a35a2, "_copy_from_user" },
+	{ 0xf64ac983, "__copy_overflow" },
+	{ 0xd272d446, "__stack_chk_fail" },
+	{ 0xd272d446, "__fentry__" },
+	{ 0xe8213e80, "_printk" },
+	{ 0xd272d446, "__x86_return_thunk" },
+	{ 0x653aa194, "class_create" },
+	{ 0xbebe66ff, "module_layout" },
+};
+
+static const u32 ____version_ext_crcs[]
+__used __section("__version_ext_crcs") = {
+	0x9f222e1e,
+	0xe486c4b7,
+	0xd5f66efd,
+	0x8ea73856,
+	0x1595e410,
+	0xa1dacb42,
+	0x4e54d6ac,
+	0x0bc5fb0d,
+	0xbd03ed67,
+	0x546c19d9,
+	0xa61fd7aa,
+	0x092a35a2,
+	0xf64ac983,
+	0xd272d446,
+	0xd272d446,
+	0xe8213e80,
+	0xd272d446,
+	0x653aa194,
+	0xbebe66ff,
+};
+static const char ____version_ext_names[]
+__used __section("__version_ext_names") =
+	"alloc_chrdev_region\0"
+	"device_create\0"
+	"cdev_init\0"
+	"cdev_add\0"
+	"device_destroy\0"
+	"class_destroy\0"
+	"cdev_del\0"
+	"unregister_chrdev_region\0"
+	"__ref_stack_chk_guard\0"
+	"validate_usercopy_range\0"
+	"__check_object_size\0"
+	"_copy_from_user\0"
+	"__copy_overflow\0"
+	"__stack_chk_fail\0"
+	"__fentry__\0"
+	"_printk\0"
+	"__x86_return_thunk\0"
+	"class_create\0"
+	"module_layout\0"
+;
+
+MODULE_INFO(depends, "");
+
+
+MODULE_INFO(srcversion, "90BFF22CCAC72125A0C69B7");
